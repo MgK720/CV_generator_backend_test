89bf13114be320d550f2e3f8f8ec10a8c92a4a8e
MgK720 <igorhg720@gmail.com>
2023-05-22 18:52:08 +0200

    big impro

--- queries.js
+++ queries.js
@@ -1,58 +1,49 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'cvgen_test',
-  password: 'password',
-  port: 5432,
+    user: 'me',
+    host: 'localhost',
+    database: 'cvgen_test',
+    password: 'password',
+    port: 5432,
 })
-
-
-//do wrzucenia w inny plik
-function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  }
-
-
 const createCv = async (request, response) => {
-    let addedCvId = 0;
+    let outputMessage = '';
     const cv_url = `http://cv/${Math.floor(Math.random() * 100000)}.cv`
-
-    pool.query('INSERT INTO cv(cv_id, create_date, cv_url) VALUES (DEFAULT, DEFAULT, $1) RETURNING *', [cv_url], (error, results) =>{
-        if (error){
-            //site something gone wrong ... error etc
-            throw error
-        }
-        addedCvId = parseInt(results.rows[0].cv_id);
-        console.log(addedCvId);
-        response.write(`Cv added with ID: ${addedCvId}\n`)
-    })
-    await sleep(1000);
-
-
-    console.log(addedCvId);
+    //personaldata zamienic na object literal
     const firstname = request.body.firstname;
     const lastname = request.body.lastname;
     const email = request.body.myemail;
     const phone_country = request.body.calling_code;
     const phone = request.body.myphone_number;
-    const img_destination = `/img/${addedCvId}`;
+    let img_destination = `/img/`;
+    try{
+        const cvID = await addCv(cv_url);
+        outputMessage += `Cv added with ID: ${cvID}\n`;
 
-    pool.query('INSERT INTO personaldata(personaldata_id, cv_id, firstname, lastname, email, phone_country, phone, img_destination) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING *',
-     [addedCvId, firstname, lastname, email,phone_country, phone,img_destination], (error, results) =>{
-        if (error){
-            throw error
-        }
-        response.write(`PersonalData added with ID: ${results.rows[0].personaldata_id}\n`)
-    })
+        img_destination += cvID; // do zmiany (rozszerzenie itp)
+        const personalDataID = await addPersonalData(cvID, firstname, lastname, email, phone_country, phone, img_destination);
+        outputMessage += `PersonalData added with ID: ${personalDataID}\n`;
 
-    
-    await sleep(1000);
-    response.status(201).send();
+        console.log(outputMessage);
+        response.status(201).send(outputMessage);
+    }catch (error){
+        console.error(error);
+        response.status(500).send("Error while creating CV");
+    }
 
   }
+const addCv = async (cv_url) =>{
+    const cvResult = await pool.query('INSERT INTO cv(cv_id, create_date, cv_url) VALUES (DEFAULT, DEFAULT, $1) RETURNING *', [cv_url]);
+    console.log(`cv_id = ${cvResult.rows[0].cv_id}`)
+    return cvResult.rows[0].cv_id;
+}
+
+const addPersonalData = async (cvID, firstname, lastname, email, phone_country, phone, img_destination)  =>{
+    const personalDataResult = await pool.query('INSERT INTO personaldata(personaldata_id, cv_id, firstname, lastname, email, phone_country, phone, img_destination) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING *',
+      [cvID, firstname, lastname, email,phone_country, phone,img_destination]);
+    console.log(`personaldata_id = ${personalDataResult.rows[0].personaldata_id}`);
+    return personalDataResult.rows[0].personaldata_id;
+}
 
   
 module.exports = {
