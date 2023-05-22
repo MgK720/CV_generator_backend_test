80a6ba232e3928d1b3dc38f5899c3fca3a062e79
MgK720 <igorhg720@gmail.com>
2023-05-22 20:02:24 +0200

    addJob query

--- queries.js
+++ queries.js
@@ -10,19 +10,37 @@ const createCv = async (request, response) => {
     let outputMessage = '';
     const cv_url = `http://cv/${Math.floor(Math.random() * 100000)}.cv`
     //personaldata zamienic na object literal
-    const firstname = request.body.firstname;
-    const lastname = request.body.lastname;
-    const email = request.body.myemail;
-    const phone_country = request.body.calling_code;
-    const phone = request.body.myphone_number;
-    let img_destination = `/img/`;
+    const personaldata = {
+         firstname : request.body.firstname,
+         lastname : request.body.lastname,
+         email : request.body.myemail,
+         phone_country : request.body.calling_code,
+         phone : request.body.myphone_number,
+         img_destination : `/img/`
+    }
+    const knowledgeArray = [
+      {
+          knowledge_name: request.body.school0, 
+          knowledgetype_id: request.body.knowledge_type0,
+          schooltype_id: request.body.school_type0,
+          start_date_knowledge: request.body.startyear0, 
+          end_date_knowledge: request.body.endyear0, 
+          description: request.body.education_description0
+      }
+    ]
     try{
+      console.log(request.body);
         const cvID = await addCv(cv_url);
         outputMessage += `Cv added with ID: ${cvID}\n`;
 
-        img_destination += cvID; // do zmiany (rozszerzenie itp)
-        const personalDataID = await addPersonalData(cvID, firstname, lastname, email, phone_country, phone, img_destination);
+        personaldata.img_destination += cvID; // do zmiany (rozszerzenie itp)
+        const personalDataID = await addPersonalData(cvID, personaldata.firstname, personaldata.lastname, personaldata.email, personaldata.phone_country, personaldata.phone, personaldata.img_destination);
         outputMessage += `PersonalData added with ID: ${personalDataID}\n`;
+        console.log(`AAAAAAAAAAAAAAAA${knowledgeArray[0].knowledgetype_id}`);
+        let knowledgeID = await addKnowledge(cvID, knowledgeArray[0].knowledge_name, knowledgeArray[0].knowledgetype_id, 
+                                            knowledgeArray[0].schooltype_id, knowledgeArray[0].start_date_knowledge, 
+                                            knowledgeArray[0].end_date_knowledge, knowledgeArray[0].description);
+        outputMessage += `Knowledge added with ID: ${knowledgeID}\n`;
 
         console.log(outputMessage);
         response.status(201).send(outputMessage);
@@ -39,10 +57,22 @@ const addCv = async (cv_url) =>{
 }
 
 const addPersonalData = async (cvID, firstname, lastname, email, phone_country, phone, img_destination)  =>{
-    const personalDataResult = await pool.query('INSERT INTO personaldata(personaldata_id, cv_id, firstname, lastname, email, phone_country, phone, img_destination) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING *',
-      [cvID, firstname, lastname, email,phone_country, phone,img_destination]);
+    const personalDataResult = await pool.query(`INSERT INTO personaldata(personaldata_id, cv_id, firstname, lastname, email, phone_country, phone, img_destination) 
+                                                 VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+                                                 [cvID, firstname, lastname, email,phone_country, phone,img_destination]);
+
     console.log(`personaldata_id = ${personalDataResult.rows[0].personaldata_id}`);
     return personalDataResult.rows[0].personaldata_id;
+}
+
+const addKnowledge = async (cvID, knowledge_name, knowledgetype_id, schooltype_id, start_date_knowledge, end_date_knowledge, description) =>{
+    if(schooltype_id === undefined) schooltype_id = null;
+    const knowledgeResult = await pool.query(`INSERT INTO knowledge(knowledge_id, cv_id, knowledge_name, knowledgetype_id, schooltype_id, start_date_knowledge, end_date_knowledge, description)
+          VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+          [cvID, knowledge_name, knowledgetype_id, schooltype_id, start_date_knowledge, end_date_knowledge, description]);
+
+    console.log(`knowledge_id = ${knowledgeResult.rows[0].knowledge_id}`);
+    return knowledgeResult.rows[0].knowledge_id;
 }
 
   
