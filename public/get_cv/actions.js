0618f06e1814726cee3921c2b92c7c07e8b70e6d
MgK720 <igorhg720@gmail.com>
2023-06-12 19:24:45 +0200

    levelbar works

--- public/get_cv/actions.js
+++ public/get_cv/actions.js
@@ -1,11 +1,11 @@
 window.onload = ()=>{
     const skillLevelDivsClassNames = ['vlow', 'low', 'medium', 'high', 'vhigh'];
-    let skillLevels = parseInt(document.querySelectorAll('levelValue'));
-    levelBar = document.querySelectorAll('.level_bar'); 
-    skillLevels.forEach(skillLevel => {
-        let count = 0;
-        for(let i = 0; i < skillLevel; i++){ 
-            levelBar[count].children[i].classList.add(skillLevelDivsClassNames[i]) 
-            } 
-    });
+    const skillLevels = document.querySelectorAll('.levelValue');
+    const levelBar = document.querySelectorAll('.level_bar'); 
+    for(let i =0; i<skillLevels.length; i++){
+        let SkillLevelValue = parseInt(skillLevels[i].innerText);
+        for(let j = 0; j < SkillLevelValue; j++){ 
+            levelBar[i].children[j].classList.add(skillLevelDivsClassNames[j])
+        } 
+    }
 }
