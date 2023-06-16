0795276ad35f216606d646984f2a291f4003d2a7
MgK720 <igorhg720@gmail.com>
2023-06-16 21:06:29 +0200

    close

--- public/scripts/handlingUpdateForm.js
+++ public/scripts/handlingUpdateForm.js
@@ -1,28 +1,31 @@
-
-
 $(document).ready(function() {
-    var myurl = {
-        firstPart: '/cv/',
-        id: '',
-        secondPart: 'update',
-        toString(){
-            return this.firstPart + this.id + this.secondPart;
-        }
-    }
-    myurl.id = window.location.pathname.replace(myurl.firstPart, '').replace(myurl.secondPart, '');
-    $.ajax({
-      url: myurl.toString(),
-      method: 'GET',
-      dataType: 'string',
-      success: function(data) {
-        const jsonData = JSON.parse(data);
-        console.log(data);
-      },
-      error: function(error, status, xhr) {
-        console.error('Error:', error);
-      console.log('Status:', status);
-      console.log('Response:', xhr.responseText);
-      }
-    });
+    console.log(outputData);
+    // Perform further processing or rendering of the data on the client side
   });
+
+// $(document).ready(function() {
+//     var myurl = {
+//         firstPart: '/cv/',
+//         id: '',
+//         secondPart: 'update',
+//         toString(){
+//             return this.firstPart + this.id + this.secondPart;
+//         }
+//     }
+//     myurl.id = window.location.pathname.replace(myurl.firstPart, '').replace(myurl.secondPart, '');
+//     $.ajax({
+//       url: myurl.toString(),
+//       method: 'GET',
+//       dataType: 'string',
+//       success: function(data) {
+//         const jsonData = JSON.parse(data);
+//         console.log(data);
+//       },
+//       error: function(error, status, xhr) {
+//         console.error('Error:', error);
+//       console.log('Status:', status);
+//       console.log('Response:', xhr.responseText);
+//       }
+//     });
+//   });
   
