777f642ae40b4a533f922b0d32c7e8797b14eb2c
MgK720 <igorhg720@gmail.com>
2023-07-10 20:31:04 +0200

    make cards frontend works

--- public/talent_finder/app.js
+++ public/talent_finder/app.js
@@ -5,22 +5,77 @@ form.addEventListener('submit', async function (e) {
     const searchTerm = form.elements.verb_like.value;
     const config = { params: { verb_like: searchTerm } }
     const res = await axios.get(`/talentfinder/search`, config);
-    //makeImages(res.data)
+    makeCards(res.data)
     console.log(res.data);
     form.elements.verb_like.value = '';
 })
 
-const makeImages = (shows) => {
-    for (let result of shows) {
-        if (result.show.image) {
-            const img = document.createElement('IMG');
-            img.src = result.show.image.medium;
-            img.classList.add('addedImg')
-            setTimeout(() =>{
-                document.body.append(img)
-            }, 3000)
-        }
+const makeCards = (data) => {
+    for (let row of data) {
+        makeCard(row);    
     }
+}
+
+const makeCard = (row) => {
+    const place = document.querySelector('#all_records');
+
+    const record = document.createElement('div')
+    record.classList.add('record');
+
+    const personaldataUl = document.createElement('ul');
+    personaldataUl.classList.add('personaldata');
+
+
+    const personaldataImgLi = document.createElement('li');
+    personaldataImgLi.classList.add('image_element');
+    const personaldataImg = document.createElement('img');
+    personaldataImg.src = row.img_destination;
+    personaldataImgLi.append(personaldataImg);
+
+    const personaldataHrefsLi = document.createElement('li');
+
+    const personaldataCvHrefParagraph = document.createElement('p');
+    const personaldataCvHref = document.createElement('a');
+    personaldataCvHref.href = `/cv/${row.cv_id}`;
+    personaldataCvHref.innerText = `${row.firstname} ${row.lastname}`;
+    personaldataCvHrefParagraph.append(personaldataCvHref);
+
+    const personaldataTelHrefParagraph = document.createElement('p');
+    const personaldataTelHref = document.createElement('a');
+    personaldataTelHref.href= `tel:+${row.phone_country}${row.phone}`;
+    personaldataTelHref.innerText = `+${row.phone_country} ${row.phone}`;
+    personaldataTelHrefParagraph.append(personaldataTelHref);
+
+    const personaldataMailHrefParagraph = document.createElement('p');
+    const personaldataMailHref = document.createElement('a');
+    personaldataMailHref.href = `mailto:${row.email}`;
+    personaldataMailHref.innerText = `${row.email}`;
+    personaldataMailHrefParagraph.append(personaldataMailHref);
+
+    personaldataHrefsLi.append(personaldataCvHrefParagraph);
+    personaldataHrefsLi.append(personaldataTelHrefParagraph);
+    personaldataHrefsLi.append(personaldataMailHrefParagraph);
+    
+    personaldataUl.append(personaldataImgLi);
+    personaldataUl.append(personaldataHrefsLi);
+
+    const skillsUl = document.createElement('ul');
+    skillsUl.classList.add('skills');
+
+
+    const skillArrayLenght = row.skills.length;
+    for(let i = 0;i<skillArrayLenght;i++){
+        const skillLi = document.createElement('li');
+        skillLi.innerText = row.skills[i];
+        skillsUl.append(skillLi);
+    }
+
+    record.append(personaldataUl);
+    record.append(skillsUl);
+
+    setTimeout(() =>{
+        place.append(record);
+    }, 3000)
 }
 
 const deleteImages = () =>{
