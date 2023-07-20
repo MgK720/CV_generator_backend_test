4c5489ff5d3b8ba77d9f67c7825e8344fb13e02b
MgK720 <igorhg720@gmail.com>
2023-07-20 16:26:14 +0200

    modal del_button DONE

--- public/get_cv/actions.js
+++ public/get_cv/actions.js
@@ -10,6 +10,16 @@ window.onload = ()=>{
     }
 }
 
+const deleteModal = document.querySelector('#delete_modal')
+const showModalButton = document.querySelector('#show_delete_modal')
+const hideModalButton = document.querySelector('#hide_modal')
+
+showModalButton.addEventListener('click', ()=>{
+    deleteModal.showModal();
+})
+hideModalButton.addEventListener('click', ()=>{
+    deleteModal.close();
+})
 
 // document.querySelector('#delete_cv_href').addEventListener('click', async(event)=>{
 //     event.preventDefault();
