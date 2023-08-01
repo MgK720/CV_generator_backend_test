19cef2f37ea58957a380a44d9f41ec14a6ce2789
MgK720 <igorhg720@gmail.com>
2023-08-01 20:18:43 +0200

    working on homepage (yt iframe)

---
+++ public/home/app.js
@@ -0,0 +1,15 @@
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: 'CP5k2gRabvc', 
+        playerVars: {},
+        events: {}
+    });
+}
