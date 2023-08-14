15fd036ca2f4deb9bb44a54ac77be3fbaf9c0480
MgK720 <igorhg720@gmail.com>
2023-08-14 20:48:20 +0200

    seeds for tests and overview DONE

---
+++ seeds/personaldata.js
@@ -0,0 +1,272 @@
+module.exports = [
+    {
+        personaldata_id: 1,
+        cv_id: 1,
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        phone_country: 123,
+        phone: 456789012,
+        img_destination: "imgs/blank1.jpg"
+      },
+      {
+        personaldata_id: 2,
+        cv_id: 2,
+        firstname: "Jane",
+        lastname: "Smith",
+        email: "jane.smith@example.com",
+        phone_country: 234,
+        phone: 567890123,
+        img_destination: "imgs/blank2.jpg"
+      },
+      {
+        personaldata_id: 3,
+        cv_id: 3,
+        firstname: "Michael",
+        lastname: "Johnson",
+        email: "michael.johnson@example.com",
+        phone_country: 345,
+        phone: 678901234,
+        img_destination: "imgs/blank3.jpg"
+      },
+      {
+        personaldata_id: 4,
+        cv_id: 4,
+        firstname: "Emily",
+        lastname: "Brown",
+        email: "emily.brown@example.com",
+        phone_country: 456,
+        phone: 789012345,
+        img_destination: "imgs/blank4.jpg"
+      },
+      {
+        personaldata_id: 5,
+        cv_id: 5,
+        firstname: "William",
+        lastname: "Jones",
+        email: "william.jones@example.com",
+        phone_country: 567,
+        phone: 890123456,
+        img_destination: "imgs/blank5.jpg"
+      },
+      {
+        personaldata_id: 6,
+        cv_id: 6,
+        firstname: "Oliver",
+        lastname: "Walker",
+        email: "oliver.walker@example.com",
+        phone_country: 789,
+        phone: 345678901,
+        img_destination: "imgs/blank6.jpg"
+      },
+      {
+        personaldata_id: 7,
+        cv_id: 7,
+        firstname: "Sophia",
+        lastname: "Garcia",
+        email: "sophia.garcia@example.com",
+        phone_country: 890,
+        phone: 456789012,
+        img_destination: "imgs/blank7.jpg"
+      },
+      {
+        personaldata_id: 8,
+        cv_id: 8,
+        firstname: "Liam",
+        lastname: "Hernandez",
+        email: "liam.hernandez@example.com",
+        phone_country: 901,
+        phone: 567890123,
+        img_destination: "imgs/blank8.jpg"
+      },
+      {
+        personaldata_id: 9,
+        cv_id: 9,
+        firstname: "Emma",
+        lastname: "Lopez",
+        email: "emma.lopez@example.com",
+        phone_country: 123,
+        phone: 678901234,
+        img_destination: "imgs/blank9.jpg"
+      },
+      {
+        personaldata_id: 10,
+        cv_id: 10,
+        firstname: "Noah",
+        lastname: "Martinez",
+        email: "noah.martinez@example.com",
+        phone_country: 234,
+        phone: 789012345,
+        img_destination: "imgs/blank10.jpg"
+      },
+      {
+        personaldata_id: 11,
+        cv_id: 11,
+        firstname: "Aiden",
+        lastname: "Gonzalez",
+        email: "aiden.gonzalez@example.com",
+        phone_country: 345,
+        phone: 890123456,
+        img_destination: "imgs/blank11.jpg"
+      },
+      {
+        personaldata_id: 12,
+        cv_id: 12,
+        firstname: "Mia",
+        lastname: "Miller",
+        email: "mia.miller@example.com",
+        phone_country: 456,
+        phone: 901234567,
+        img_destination: "imgs/blank12.jpg"
+      },
+      {
+        personaldata_id: 13,
+        cv_id: 13,
+        firstname: "Lucas",
+        lastname: "Jackson",
+        email: "lucas.jackson@example.com",
+        phone_country: 567,
+        phone: 123456789,
+        img_destination: "imgs/blank13.jpg"
+      },
+      {
+        personaldata_id: 14,
+        cv_id: 14,
+        firstname: "Isabella",
+        lastname: "Adams",
+        email: "isabella.adams@example.com",
+        phone_country: 678,
+        phone: 234567890,
+        img_destination: "imgs/blank14.jpg"
+      },
+      {
+        personaldata_id: 15,
+        cv_id: 15,
+        firstname: "Henry",
+        lastname: "King",
+        email: "henry.king@example.com",
+        phone_country: 789,
+        phone: 345678901,
+        img_destination: "imgs/blank15.jpg"
+      },
+      {
+        personaldata_id: 16,
+        cv_id: 16,
+        firstname: "Sophia",
+        lastname: "Johnson",
+        email: "sophia.johnson@example.com",
+        phone_country: 890,
+        phone: 456789012,
+        img_destination: "imgs/blank16.jpg"
+      },
+      {
+        personaldata_id: 17,
+        cv_id: 17,
+        firstname: "Jackson",
+        lastname: "Lee",
+        email: "jackson.lee@example.com",
+        phone_country: 901,
+        phone: 567890123,
+        img_destination: "imgs/blank17.jpg"
+      },
+      {
+        personaldata_id: 18,
+        cv_id: 18,
+        firstname: "Olivia",
+        lastname: "Young",
+        email: "olivia.young@example.com",
+        phone_country: 123,
+        phone: 678901234,
+        img_destination: "imgs/blank18.jpg"
+      },
+      {
+        personaldata_id: 19,
+        cv_id: 19,
+        firstname: "Liam",
+        lastname: "Wright",
+        email: "liam.wright@example.com",
+        phone_country: 234,
+        phone: 789012345,
+        img_destination: "imgs/blank19.jpg"
+      },
+      {
+        personaldata_id: 20,
+        cv_id: 20,
+        firstname: "Emma",
+        lastname: "Turner",
+        email: "emma.turner@example.com",
+        phone_country: 345,
+        phone: 890123456,
+        img_destination: "imgs/blank20.jpg"
+      },
+      {
+        job_id: 31,
+        cv_id: 8,
+        job_name: "Data Scientist",
+        start_date_job: "2021-03-15",
+        end_date_job: "2023-06-30"
+      },
+      {
+        job_id: 32,
+        cv_id: 7,
+        job_name: "Machine Learning Engineer",
+        start_date_job: "2023-09-01",
+        end_date_job: "2024-12-31"
+      },
+      {
+        job_id: 33,
+        cv_id: 6,
+        job_name: "Nurse",
+        start_date_job: "2020-08-01",
+        end_date_job: "2022-11-30"
+      },
+      {
+        job_id: 34,
+        cv_id: 1,
+        job_name: "Civil Engineer",
+        start_date_job: "2019-09-15",
+        end_date_job: "2023-12-31"
+      },
+      {
+        job_id: 35,
+        cv_id: 10,
+        job_name: "Architect",
+        start_date_job: "2021-01-01",
+        end_date_job: "2024-04-30"
+      },
+      {
+        job_id: 36,
+        cv_id: 12,
+        job_name: "English Teacher",
+        start_date_job: "2020-05-15",
+        end_date_job: "2023-08-31"
+      },
+      {
+        job_id: 37,
+        cv_id: 14,
+        job_name: "Clinical Psychologist",
+        start_date_job: "2019-04-01",
+        end_date_job: "2022-07-31"
+      },
+      {
+        job_id: 38,
+        cv_id: 16,
+        job_name: "Therapist",
+        start_date_job: "2021-02-15",
+        end_date_job: "2023-05-31"
+      },
+      {
+        job_id: 39,
+        cv_id: 18,
+        job_name: "History Researcher",
+        start_date_job: "2020-03-01",
+        end_date_job: "2023-06-30"
+      },
+      {
+        job_id: 40,
+        cv_id: 15,
+        job_name: "Elementary School Teacher",
+        start_date_job: "2021-07-15",
+        end_date_job: "2024-10-31"
+      }
+]
